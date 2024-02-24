77f015e487056c7fdceba81a86413ed9390276e6
Ripon Shikder <riponoffice121@gmail.com>
2024-02-24 12:45:03 +0600

    migration modify

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory,HasUuids;
+    use HasFactory, HasUuids;
     protected $primaryKey = 'product_id';
-    protected $fillable =['product_name'];
+    protected $fillable = ['product_name'];
 }
