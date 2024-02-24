cf3376ef4b9931aec634dd9b8f44d855e35aae33
Ripon Shikder <riponoffice121@gmail.com>
2024-02-24 12:15:01 +0600

    create model with seeder

--- database/migrations/2024_02_24_054112_create_orders_table.php
+++ database/migrations/2024_02_24_054112_create_orders_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('product_id')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,23 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Order;
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class OrderSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $product = Product::first();
-        $user = User::first();
-        Order::create([
-            "user_id" => $user->id,
-            "product_id" => $product->id,
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
